032e7404dc3b8288a6ba4d6fd8cdfef180116ba2
alexcontreras7 <68401782+alexcontreras7@users.noreply.github.com>
2020-10-26 19:45:05 -0700

    Add files via upload

---
+++ Assignment 2/circleCode.py
@@ -0,0 +1,24 @@
+# Move the robot forwards and backwards
+from romipi_astar.romipi_driver import AStar
+import time
+
+motor_delay_s = 5.0
+forward_speed_m_s = 0.25
+stop_speed_m_s = 1.25
+romi = AStar()
+
+# int main()
+# turn off all LEDs
+romi.leds(False,False,False)
+romi.pixels(0,0,0)
+print("Displaying Circle Code")
+var = input("Press s/S to stop and any key to continue: ")
+while var != "s" and var != "S":
+	currentTime = time.time()
+	nextTime = currentTime + motor_delay_s
+	while currentTime < nextTime:
+		romi.twist(forward_speed_m_s, stop_speed_m_s)
+ 		currentTime = time.time()
+	romi.twist(0.0, 0.0)
+	var = input("Press s/S to stop and any key to continue: ")
+
